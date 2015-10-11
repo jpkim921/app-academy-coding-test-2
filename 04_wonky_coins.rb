fd9363f2dcdd5de8200527b34c9f65dbf7268423
chrislample <clample@crimson.ua.edu>
2015-10-11 23:41:31 +0000

    solved problem 5

--- 04_wonky_coins.rb
+++ 04_wonky_coins.rb
@@ -1,2 +1,24 @@
+def change_coins(coin_array)
+    #Takes a coin and returns the three values of new coins you're given
+    new_coin_array = Array.new
+    coin_array.each do |coin|
+        if coin != 0
+            new_coin_array.push(coin/2)
+            new_coin_array.push(coin/3)
+            new_coin_array.push(coin/4)
+        else
+            new_coin_array.push(coin)
+        end
+    end
+    return new_coin_array
+end
+
 def wonky_coins(n)
+    coin_array = [n]
+    while true
+        coin_array = change_coins(coin_array)
+        if coin_array == Array.new(coin_array.length, 0) #Check if all the coins are zero yet
+            return coin_array.length
+        end
+    end
 end
