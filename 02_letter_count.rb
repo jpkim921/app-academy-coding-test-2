69820ca0f08efee30866cf22ddd0135b1024e299
chrislample <clample@crimson.ua.edu>
2015-10-11 22:57:20 +0000

    solved problem 3

--- 02_letter_count.rb
+++ 02_letter_count.rb
@@ -1,2 +1,14 @@
 def letter_count(str)
+    frequency_hash = Hash.new
+    str = str.delete(" ").split("")
+    str.each do |letter|
+        if frequency_hash.has_key?(letter)
+            frequency_hash[letter] += 1
+        else
+            frequency_hash[letter] = 1
+        end
+    end
+    return frequency_hash
 end
+
+#puts letter_count("mmmmyyyp").to_s
