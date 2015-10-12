8069e8a57867ced4beb0187710af7ecfb71a93d3
chrislample <clample@crimson.ua.edu>
2015-10-12 03:11:58 +0000

    solved problem 7

--- 06_word_unscrambler.rb
+++ 06_word_unscrambler.rb
@@ -1,2 +1,9 @@
 def word_unscrambler(str, words)
+    annagrams = Array.new
+    words.each do |word|
+        if str.split("").sort == word.split("").sort
+            annagrams.push(word)
+        end
+    end
+    return annagrams
 end
