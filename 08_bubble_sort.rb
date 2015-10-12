c3c245a141a89fd613815cddec24c477a2545614
chrislample <clample@crimson.ua.edu>
2015-10-12 04:20:15 +0000

    implemented bubble sort

--- 08_bubble_sort.rb
+++ 08_bubble_sort.rb
@@ -1,2 +1,18 @@
 def bubble_sort(arr)
+    no_swaps = true
+    while true
+        i = 0
+        no_swaps = true
+        while i + 1< arr.length
+            if arr[i+1] < arr[i]
+                val = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = val
+                no_swaps = false
+            end
+            i += 1
+        end
+        break if no_swaps  
+    end
+    return arr
 end
